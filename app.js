21b75a1175ddf7173e84fa5a0cff73dfdef10a98
jlmartin9 <jamesml@live.unc.edu>
2014-08-06 20:26:49 -0400

    API working, need to get asynchronous calls down.

--- app.js
+++ app.js
@@ -6,17 +6,31 @@ var socketio = require('socket.io');
 var github = require('octonode');
 var client = github.client();
 
-// Getting tired of typing 'console.log'
-var p = function(d){
+var p = function(d) {
     console.log(d);
 }
 
-var ghme = client.me();
+//Testing repo API
 var ghrepo = client.repo('jlmartin9/CIHSP');
 
-ghrepo.contributors(function() {
+var lang;
 
+ghrepo.languages(function(err, data, headers) {
+    // console.log("error: " + err);
+    // console.log("headers:" + headers);
+    lang = data;
+    p("Set!");
+    p(lang);
 });
+
+var info;
+
+ghrepo.info(function(err, data, headers) {
+    info = data.created_at;
+    p(info);
+});
+
+//Start Express and Socket.io
 
 var express = require('express');
 var app = express();
@@ -34,8 +48,8 @@ io.sockets.on('connection', function(socket) {
     /**
      * getLang(lang)
      * @param var lang - language to retrieve stastics for
-     * @return 2D array - returns an array with first column year,
-     * second column percentage
+     * @return 2D array - returns an array with first column date,
+     *                                          second column percentage
      */
     socket.on('getLang', function(lang) {
         //console.log('Test!');
@@ -43,8 +57,9 @@ io.sockets.on('connection', function(socket) {
     });
 
     socket.on('getRepo', function(repo) {
-    	console.log(repo);
+        console.log(repo);
     });
+    
     /**
      * getRepoBreakdown(lang)
      * @param var lang - language to retrieve breakdown for
